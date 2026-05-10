8cb29744a926b8e56fb34d68275c6874d5029a1e
SongChiChuang <iness1029@gmail.com>
2026-05-10 17:25:35 +0800

    Create logger.py
    build logger utility

---
+++ utils/logger.py
@@ -0,0 +1,19 @@
+# =========================================
+# Logger
+# =========================================
+
+def log_info(msg):
+
+    print(f"ℹ️ INFO: {msg}")
+
+def log_success(msg):
+
+    print(f"✅ SUCCESS: {msg}")
+
+def log_warning(msg):
+
+    print(f"⚠️ WARNING: {msg}")
+
+def log_error(msg):
+
+    print(f"❌ ERROR: {msg}")
